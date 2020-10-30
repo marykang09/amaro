4623de0c69d54b38913940b3cedf6305062ce0c4
marykang09 <mary.kang09@gmail.com>
2020-10-30 14:46:13 -0400

    the future mr and mrs baldo

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -12,7 +12,7 @@ class Events extends Component {
                 <p className="infoLabel"><span> when: </span> <em className="infoText"> October 31, 2020 </em></p>
                 <p className="infoLabel"><span> where: </span><em className="infoText"> in home tasting, on Leithgow  </em></p>
                 <br></br>
-                <p className="infoLabel"><span></span><em className="infoText"> Mixologist Alex Bevis and his assistant Mary Kang are excited to present an intimate and memorable amaro tasting for their dearest friends, Nick and Mal. </em></p>
+                <p className="infoLabel"><span></span><em className="infoText"> Mixologist Alex Bevis and his assistant Mary Kang are excited to present an intimate and memorable amaro tasting for their dearest friends, Nick and Mal - the future Mr. and Mrs. Baldo! </em></p>
 
             </section>
         )
