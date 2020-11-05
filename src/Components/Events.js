4e20f29ab51e5ae7a633e22f348665ea9ea4d188
marykang09 <mary.kang09@gmail.com>
2020-11-05 14:57:59 -0500

    events update

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -6,14 +6,21 @@ class Events extends Component {
     render(){
         return(
             <section id="events">
-                <Link to="/nickandmal">
-                    <h1> Amaro Tasting for Nick & Mal </h1>
-                </Link>
-                <p className="infoLabel"><span> when: </span> <em className="infoText"> October 31, 2020 </em></p>
-                <p className="infoLabel"><span> where: </span><em className="infoText"> in home tasting, on Leithgow  </em></p>
-                <br></br>
-                <p className="infoLabel"><span></span><em className="infoText"> Mixologist Alex Bevis and his assistant Mary Kang are excited to present an intimate and memorable amaro tasting for their dearest friends, Nick and Mal - the future Mr. and Mrs. Baldo! </em></p>
-
+                <div className="upcoming">
+                    <h1> UPCOMING EVENTS </h1><br></br>
+                    <h2> There are no upcoming events. </h2>
+                </div>
+                <div className="line"></div>
+                <div className="past">
+                    <h1> PAST EVENTS </h1><br></br>
+                    <Link to="/nickandmal">
+                        <h2> Amaro Tasting for Nick & Mal </h2>
+                    </Link>
+                    <p className="infoLabel"><span> when: </span> <em className="infoText"> October 31, 2020 </em></p>
+                    <p className="infoLabel"><span> where: </span><em className="infoText"> in home tasting, on Leithgow  </em></p>
+                    <br></br>
+                    <p className="infoLabel"><span></span><em className="infoText"> Mixologist Alex Bevis and his assistant Mary Kang are excited to present an intimate and memorable amaro tasting for their dearest friends, Nick and Mal - the future Mr. and Mrs. Baldo! </em></p>
+                </div>
             </section>
         )
     }
